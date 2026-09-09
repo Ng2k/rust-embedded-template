123a36fabbb1f093fb141356fe0f1252ba8925e1
Nicola Guerra <nicola.ng2k@gmail.com>
2026-09-09 11:51:05 +0200

    feat: add ESP32 firmware template

---
+++ firmware/esp32/src/bin/main.rs
@@ -0,0 +1,38 @@
+#![no_std]
+#![no_main]
+
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+#![deny(clippy::large_stack_frames)]
+
+use esp_hal::{
+    clock::CpuClock,
+    main,
+    time::{Duration, Instant},
+};
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[allow(
+    clippy::large_stack_frames,
+    reason = "it's not unusual to allocate larger buffers etc. in main"
+)]
+#[main]
+fn main() -> ! {
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let _peripherals = esp_hal::init(config);
+
+    loop {
+        let delay_start = Instant::now();
+
+        while delay_start.elapsed() < Duration::from_millis(500) {}
+    }
+}
