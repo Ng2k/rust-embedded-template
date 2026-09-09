b5d859840a52e6ee5971311e5861e88f8cbba400
Nicola Guerra <nicola.ng2k@gmail.com>
2026-09-09 17:45:48 +0200

    ci: add GitHub Actions checks

--- crates/logger/src/lib.rs
+++ crates/logger/src/lib.rs
@@ -2,36 +2,72 @@
 
 #[cfg(feature = "host")]
 #[doc(hidden)]
-pub use tracing as __backend;
+pub use tracing as __tracing;
 
 #[cfg(feature = "embedded")]
 #[doc(hidden)]
-pub use defmt as __backend;
+pub use defmt as __defmt;
 
+#[cfg(feature = "host")]
 #[macro_export]
 macro_rules! debug {
     ($($arg:tt)*) => {
-        $crate::__backend::debug!($($arg)*)
+        $crate::__tracing::debug!($($arg)*);
     };
 }
 
+#[cfg(feature = "embedded")]
+#[macro_export]
+macro_rules! debug {
+    ($($arg:tt)*) => {
+        $crate::__defmt::debug!($($arg)*);
+    };
+}
+
+#[cfg(feature = "host")]
 #[macro_export]
 macro_rules! info {
     ($($arg:tt)*) => {
-        $crate::__backend::info!($($arg)*)
+        $crate::__tracing::info!($($arg)*);
     };
 }
 
+#[cfg(feature = "embedded")]
+#[macro_export]
+macro_rules! info {
+    ($($arg:tt)*) => {
+        $crate::__defmt::info!($($arg)*);
+    };
+}
+
+#[cfg(feature = "host")]
 #[macro_export]
 macro_rules! warn {
     ($($arg:tt)*) => {
-        $crate::__backend::warn!($($arg)*)
+        $crate::__tracing::warn!($($arg)*);
     };
 }
 
+#[cfg(feature = "embedded")]
+#[macro_export]
+macro_rules! warn {
+    ($($arg:tt)*) => {
+        $crate::__defmt::warn!($($arg)*);
+    };
+}
+
+#[cfg(feature = "host")]
 #[macro_export]
 macro_rules! error {
     ($($arg:tt)*) => {
-        $crate::__backend::error!($($arg)*)
+        $crate::__tracing::error!($($arg)*);
+    };
+}
+
+#[cfg(feature = "embedded")]
+#[macro_export]
+macro_rules! error {
+    ($($arg:tt)*) => {
+        $crate::__defmt::error!($($arg)*);
     };
 }
